b64a6650676866d2d65052b5d76438297c4dd2c8
17301076phc <17301076@bjtu.edu.cn>
2019-11-30 17:01:17 +0800

    Merge branch 'master' of https://github.com/17301076phc/AndroidSecondPrj
    # Conflicts:
    #	.gitignore

---
+++ app/src/main/java/com/example/firstassignment/CourseIndro.java
@@ -0,0 +1,27 @@
+package com.example.firstassignment;
+
+public class CourseIndro {
+    private String course_name_teacher;
+
+    private String course_illustrate;
+
+    private String course_time;
+
+    private String time;
+
+    public String getCourse_name_teacher() {
+        return course_name_teacher;
+    }
+
+    public String getCourse_illustrate() {
+        return course_illustrate;
+    }
+
+    public String getCourse_time() {
+        return course_time;
+    }
+
+    public String getTime() {
+        return time;
+    }
+}
---
+++ app/src/main/java/com/example/firstassignment/MyBroadCastReceiver.java
@@ -0,0 +1,15 @@
+package com.example.firstassignment;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MyBroadCastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("tt","yyyyyyyyy");
+    }
+}
